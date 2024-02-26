f90838fb0215114a6916accd05e4afd8e8184df0
unknown <dschenck@win.redhat.com>
2024-02-26 09:45:09 -0500

    Added five-minute timeout

--- server.js
+++ server.js
@@ -45,4 +45,5 @@ server = app.listen(port, () => {
     logger.info(`Node server is running on port ${port} at ${new Date()}`);
     logger.info('Starting pressure')
     promiseExecution();
+    process.exit();
 });
